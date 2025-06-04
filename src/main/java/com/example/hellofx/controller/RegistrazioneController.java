c42c3e776028f93e9182bdc0d70c6f0d5cd91197
Gianluca Quaranta <gianluca.q.rm@gmail.com>
2025-06-04 13:47:05 +0200

    RegistrazioneController

--- src/main/java/com/example/hellofx/controller/RegistrazioneController.java
+++ src/main/java/com/example/hellofx/controller/RegistrazioneController.java
@@ -1,6 +1,33 @@
 package com.example.hellofx.controller;
 
+import com.example.hellofx.bean.RegistrazioneBibliotecaBean;
+import com.example.hellofx.bean.RegistrazioneUtenteBean;
+
+import java.util.List;
+
 public class RegistrazioneController {
 
-    private static final String MEMORY = "memory";
+    public Boolean registraBiblioteca(RegistrazioneBibliotecaBean rbb){
+        return checkCredenziali(rbb.getNome(), "bib");
+    }
+
+    public Boolean registraUtente(RegistrazioneUtenteBean rub){
+        return checkCredenziali(rub.getUsername(), "utente");
+    }
+
+    private Boolean checkCredenziali(String username, String type){
+        RegistrazioneDao rd = new RegistrazioneDao();
+        List<String> usernames = rd.loadAll(type);
+
+        for(String u : usernames) {
+            if (u.equals(username)) {
+                return false;
+            }
+        }
+
+        rd.store(username);
+        return true;
+
+    }
+
 }
