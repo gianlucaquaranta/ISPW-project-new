b61fe818d38807d1c8d3ba3b1ca62ae24945fb33
BeatriceProietti <bea.pro.03@gmail.com>
2025-06-04 15:49:52 +0200

    RegistrazioneDAO, RegistrazioneController - creazione e store di nuove entity

--- src/main/java/com/example/hellofx/controller/RegistrazioneController.java
+++ src/main/java/com/example/hellofx/controller/RegistrazioneController.java
@@ -2,33 +2,106 @@ package com.example.hellofx.controller;
 
 import com.example.hellofx.bean.RegistrazioneBibliotecaBean;
 import com.example.hellofx.bean.RegistrazioneUtenteBean;
+import com.example.hellofx.dao.FactoryProducer;
+import com.example.hellofx.dao.bibliotecadao.BibliotecaDao;
 import com.example.hellofx.dao.registrazionedao.RegistrazioneDao;
+import com.example.hellofx.dao.registrazionedao.RegistrazioneDaoFile;
+import com.example.hellofx.dao.registrazionedao.RegistrazioneDaoMemory;
+import com.example.hellofx.dao.utentedao.UtenteDao;
+import com.example.hellofx.model.Biblioteca;
+import com.example.hellofx.model.Posizione;
+import com.example.hellofx.model.Utente;
+import com.example.hellofx.model.modelfactory.BibliotecaFactory;
+import com.example.hellofx.model.modelfactory.UtenteFactory;
+import com.example.hellofx.session.Session;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class RegistrazioneController {
+    private static final String MEMORY = "memory";
+    private static final String FILE = "file";
+    BibliotecaDao bibliotecaDaoMemory = FactoryProducer.getFactory(MEMORY).createDaoBiblioteca();
+    UtenteDao utenteDaoMemory = FactoryProducer.getFactory(MEMORY).createDaoUtente();
 
-    public boolean registraBiblioteca(RegistrazioneBibliotecaBean rbb){
-        return checkCredenziali(rbb.getNome(), "bib");
+    public boolean registraBiblioteca(RegistrazioneBibliotecaBean rbb) {
+         boolean result = checkCredenziali(rbb.getNome(), "bib");
+         if(!result){
+             return false;
+         }
+         Biblioteca b = BibliotecaFactory.getInstance().createBiblioteca();
+         b.setNome(rbb.getNome());
+         b.setCatalogo(new ArrayList<>());
+         b.setPosizione(new Posizione(rbb.getCap(), rbb.getIndirizzo(), rbb.getNumeroCivico(), rbb.getCitta(), rbb.getProvincia()));
+         b.setPrenotazioniAttive(new ArrayList<>());
+         b.setCopie(new HashMap<>());
+         b.setNoleggiAttivi(new ArrayList<>());
+         b.setId(rbb.getNome());
+
+         bibliotecaDaoMemory.update(b);
+         if(Session.isFull()){
+            BibliotecaDao bibliotecaDaoFile = FactoryProducer.getFactory(FILE).createDaoBiblioteca();
+            bibliotecaDaoFile.update(b);
+         }
+
+         return true;
     }
 
-    public boolean registraUtente(RegistrazioneUtenteBean rub){
-        return checkCredenziali(rub.getUsername(), "utente");
-    }
+    public boolean registraUtente(RegistrazioneUtenteBean rub) {
+        boolean result = checkCredenziali(rub.getUsername(), "utente");
 
-    private boolean checkCredenziali(String username, String type){
-        RegistrazioneDao rd = new RegistrazioneDao();
-        List<String> usernames = rd.loadAll(type);
+        if(!result){
+            return false;
+        }
+        Utente u = UtenteFactory.getInstance().createUtente();
+        u.setUsername(rub.getUsername());
+        u.setEmail(rub.getEmail());
+        u.setNoleggiAttivi(new ArrayList<>());
+        u.setPrenotazioniAttive(new ArrayList<>());
+        u.setRicercheSalvate(new ArrayList<>());
 
-        for(String u : usernames) {
-            if (u.equals(username)) {
-                return false;
+        utenteDaoMemory.storeUtente(u);
+        if(Session.isFull()){
+            if(Session.isFile()){
+                UtenteDao utenteDaoFile = FactoryProducer.getFactory(FILE).createDaoUtente();
+                utenteDaoFile.storeUtente(u);
+            } else {
+                UtenteDao utenteDaoDb = FactoryProducer.getFactory("db").createDaoUtente();
+                utenteDaoDb.storeUtente(u);
             }
         }
 
-        rd.store(type, username);
+        return true;
+    }
+
+    private boolean checkCredenziali(String username, String type) { //true se non trova credenziali già salvate
+        RegistrazioneDao rdm = RegistrazioneDaoMemory.getInstance();
+
+        if(found(rdm.loadAll(type), username)) {
+            return false;
+        }
+
+        if (Session.isFull()) {
+            RegistrazioneDaoFile rdf = new RegistrazioneDaoFile();
+            if(found(rdf.loadAll(type), username)) {
+                return false;
+            }
+            rdf.store(type, username);
+        }
+
+        rdm.store(type, username);
         return true;
 
+    }
+
+    boolean found(List<String> list, String username) { //nome trovato nella lista
+        for (String u : list) {
+            if (u.equals(username)) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
